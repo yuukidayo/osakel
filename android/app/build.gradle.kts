3faa5e9a90570bfbff9e6b38ed97ab138cfb5603
admin <sunline.swift@gmail.com>
2025-06-29 08:44:57 +0900

    fix: Bundle IDの統一化
    - AndroidのapplicationIdとnamespaceを「com.osakel.testapp」から「com.osakel.app」に変更
    - GoogleService-Info.plistとgoogle-services.jsonのBundleIDと整合性を確保
    - Firebase設定ファイルとアプリのBundleIDを「com.osakel.app」に統一

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 android {
-    namespace = "com.osakel.testapp"
+    namespace = "com.osakel.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973" // Updated to match Firebase plugin requirements
 
@@ -23,7 +23,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.osakel.testapp"
+        applicationId = "com.osakel.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21 // Firebase requires a minimum SDK version of 19, but we're using 21 for better compatibility
